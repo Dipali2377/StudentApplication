f901c63c84b470b1e5eb75e7bf045d75ec4815c8
Dipali-art <67815632+Dipali-art@users.noreply.github.com>
2022-04-08 15:43:30 +0530

    Dipali
    abcsjsbc

---
+++ StudentApplication/Controllers/GithubDemoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentApplication.Controllers
+{
+    public class GithubDemoController : Controller
+    {
+        //
+        // GET: /GithubDemo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
