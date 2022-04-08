5c70fc8e7851251e970e45cc9deecb86259b78c5
Dipali-art <67815632+Dipali-art@users.noreply.github.com>
2022-04-08 15:05:07 +0530

    Initial commit

---
+++ StudentApplication/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentApplication.Models;
+
+namespace StudentApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ StudentApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentApplication.MvcApplication" Language="C#" %>
